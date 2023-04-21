4f312aaac17c94a9f77ffb6fd544f2fd6fc5f8fa
Akpehglo1993 <Akpehorimglo@gmail.com>
2023-04-21 06:38:36 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -program that used print f
+ * Return: 0 (success)
+ */
+int main(0)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
