6b2899ae39d652bdb700717d12ab27bbbcf745d1
Akpehglo1993 <Akpehorimglo@gmail.com>
2023-04-28 06:30:41 -0700

    ./6-lines | cat -e

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,7 @@
 
 /**
  * print_line - draws a straight line in the terminal
- * @n: number of times the character _ should be printed
+ * @n: number of times the character_should be printed
  */
 void print_line(int n)
 {
@@ -13,7 +13,7 @@ void print_line(int n)
 	{
 		int i;
 
-		for (i == 1; i <= n; i++)
+		for (i = 1; i <= n; i++)
 		{
 			_putchar('_');
 		}
