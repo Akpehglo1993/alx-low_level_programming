499bf390444487e0a9209bea2f9a4fbd34b43545
Akpehglo1993 <Akpehorimglo@gmail.com>
2023-06-13 09:27:55 -0700

    Task4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -18,7 +18,7 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 
 /**
- * check_elf - information contained in the ELF header at the start of an ELF file.
+ * check_elf - information contained in the ELF header.
  * @e_ident: A pointer to an array containing the ELF magic numbers.
  *
  * Description: If the file is not an ELF file - exit code 98.
